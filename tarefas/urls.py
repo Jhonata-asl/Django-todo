a638bb1e316e2e1f895cc329d39915b1b9f144f9
Jhonata-asl <jhonatasilvaaraujo67@gmail.com>
2025-04-01 21:41:40 -0300

    versao7
    altualização

--- tarefas/urls.py
+++ tarefas/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
-urlpatterns=[
-    path('', views.listaTarefa, name='lista-tarefa')
+urlpatterns = [
+    path('', views.listaTarefa, name='lista-tarefa'),
+    path('tarefa/<int:id>', views.tarefaView, 
+         name='tarefa-view'),
+    path('novaTarefa/', views.novaTarefa, name='nova-tarefa'),
 ]
