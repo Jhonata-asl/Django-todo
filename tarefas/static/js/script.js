d43b84abf5fdf0a8009c4a50ef9d717da523277e
Jhonata-asl <jhonatasilvaaraujo67@gmail.com>
2025-04-02 21:44:20 -0300

    versão8
    atualizaçao

--- tarefas/static/js/script.js
+++ tarefas/static/js/script.js
@@ -1,1 +1,16 @@
-console.log("Fucionou");
+//Função de Pergunta ao usuário se ele quer excluir uma tarefa
+
+document.querySelectorAll(".delete-btn").forEach(
+    btn => {
+        btn.addEventListener("click", function(e){
+            e.preventDefault();
+
+            const delLink = this.getAttribute("href");
+
+            if(delLink && confirm("Quer deletar esta tarefa??"))
+            {
+                window.location.href = delLink;
+            }
+     });
+    }
+)
